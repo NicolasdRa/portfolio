74669866018248abd60feddae81be1ec40367cba
Nicolas <nicolasdirago@gmail.com>
2021-09-29 16:09:06 +0200

    styled placeholder pages for detailed about and porjects pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { gsap } from 'gsap';
+import { Link, PageProps } from 'gatsby';
+import { theme } from '../style/theme';
 
-const About = () => <div>About Page full info</div>;
+const Wrapper = styled.main`
+  h1 {
+    font-family: ${theme.fonts.secondary};
+    font-size: 4rem;
+    font-weight: lighter;
+    margin-bottom: 5rem;
+  }
+`;
+
+const About: React.FC<PageProps> = () => {
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!contentRef.current) throw Error('divRef is not assigned');
+    const element = contentRef.current;
+    gsap.fromTo(element, { opacity: 0 }, { duration: 0.8, ease: 'expo.out', opacity: 1 });
+  }, []);
+
+  return (
+    <Wrapper className="error-page">
+      <div ref={contentRef} className="error-container">
+        <h1>a detailed about page is coming soon...</h1>
+        <Link to="/" className="cta">
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default About;
