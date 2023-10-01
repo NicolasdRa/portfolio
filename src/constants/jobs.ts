e42a5cfd859297046d4d2d38b4f7069c40b008c5
Nicolas di Rago <nicolasdirago@gmail.com>
2023-10-01 21:41:06 +0200

    added job experience

--- src/constants/jobs.ts
+++ src/constants/jobs.ts
@@ -1,6 +1,17 @@
 const data = [
   {
     id: 1,
+    company: 'Nuuk GmbH',
+    position: 'frontend developer',
+    startDate: 'June 2023',
+    endDate: 'present',
+    tasks: [
+      'Developed project from scratch for the biggest cultural event in Germany. Stack: NextJS 13, Typescript, Tailwind CSS, Graphql, Google Maps API, Contentful CMS, Vercel.',
+      'Maintained project for German editorial house built in NextJS, Typescript, Tailwind CSS, Graphql, Contentful CMS & Vercel.',
+    ],
+  },
+  {
+    id: 2,
     company: 'supertemplates UG',
     position: 'co-founder & developer',
     startDate: 'November 2021',
@@ -14,15 +25,15 @@ const data = [
     ],
   },
   {
-    id: 2,
+    id: 3,
     company: 'civilisten gmbH',
     position: 'frontend developer',
     startDate: 'April 2021',
-    endDate: 'present',
+    endDate: 'Novemeber 2022',
     tasks: ['Developed responsive HTML email templates tested for 90+ email clients and devices.'],
   },
   {
-    id: 3,
+    id: 4,
     company: 'energenious gmbH',
     position: 'frontend developer',
     startDate: 'November 2020',
@@ -35,7 +46,7 @@ const data = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     company: 'EO technologies',
     position: 'frontend developer',
     startDate: 'February 2021',
