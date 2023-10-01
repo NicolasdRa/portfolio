f2dce955def20f383deb5d0d0937f2329326084e
Nicolas di Rago <nicolasdirago@gmail.com>
2023-10-01 21:50:28 +0200

    modified stack

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -1,7 +1,20 @@
 const data = {
   text: 'i make things for the web, find solutions and create visual and conceptual identities. in the last years, i’ve been cooperating with start-ups to design, elaborate on user flows and develop websites, apps and microservices. currently focused on the frontend side though able to deal with backend solutions too. this is my main technology stack:',
 
-  stack: ['JavaScript (ES6+)', 'TypeScript', 'React', 'Gatsby', 'Vue', 'Node.js', 'WordPress'],
+  stack: [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Next JS',
+    'Gatsby',
+    'Vue',
+    'Node.js',
+    'Php',
+    'WordPress',
+    'Contentful',
+    'GraphQL',
+    'REST API',
+  ],
 };
 
 export default data;
